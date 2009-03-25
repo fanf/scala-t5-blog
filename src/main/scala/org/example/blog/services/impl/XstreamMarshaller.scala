6937cd256feef0053115d0e7a0ba3cfbc4456e91
fanf <fanf@barnard.linagora.lan>
2009-03-25 15:55:22 +0100

    Configure output for the Article object

--- src/main/scala/org/example/blog/services/impl/XstreamMarshaller.scala
+++ src/main/scala/org/example/blog/services/impl/XstreamMarshaller.scala
@@ -30,5 +30,37 @@ class XmlXstreamMarshaller() extends XstreamMarshaller(new XStream(new DomDriver
 
 class JsonXstreamMarshaller() extends XstreamMarshaller(new XStream(new JsonHierarchicalStreamDriver()))
 
+object ConfigureArticle4Xstream {
+  def configure(x:XStream) {
+    import org.example.blog.data.{Article,NoneArticle}
+    //output "article" in place of org.example.blog.data.Article
+    x.alias("article",classOf[Article])
+    
+    //title and id will be output as attribute of article...
+    x.useAttributeFor(classOf[Article],"title")
+    x.useAttributeFor(classOf[Article],"id")
 
+    //...and since Id is not a simple string, we have to provide a converter to make it works
+    x.registerLocalConverter(classOf[Article],"id",new ArticleIdConverter())
+    
+    //we don't want to display comments
+    x.omitField(classOf[Article],"comments")
+    
+    //special config for NoneArticle
+    x.alias("article",NoneArticle.getClass)
+  }
+}
 
+import com.thoughtworks.xstream.converters.SingleValueConverter
+class ArticleIdConverter extends SingleValueConverter {
+  def fromString(s:String) = if("none" == s) None else Some(s)
+  def toString(a:Object) = a match { 
+    case None => "none" 
+    case Some(x) => x.toString 
+    case _ => error("Not supported type: " + a.getClass.getName)
+  }
+  def canConvert(c:Class[_]) = {
+    if(c == classOf[scala.Option[_]]) true
+    else false
+  }  
+}
